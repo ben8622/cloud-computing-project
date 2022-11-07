ae883eaafca03745f958641f18bdad3eac6ba362
Benjamin Barani <benbarani@hotmail.com>
2022-11-06 18:12:33 -0600

    Added new signup app, added a view and set up the urls.py to route to /signup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-le3d3x47fpb1v44r#$@d1b8!-zo3v-)m9xeha^r1l_ydk8o08n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
