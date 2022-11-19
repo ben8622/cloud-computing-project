acdb6f4f42d0c76261ef96709d3c9c40370c97ae
ben8622 <benjaminjknight0987@gmail.com>
2022-11-19 14:09:35 -0600

    removed secret key

--- backend/tothemoon/tothemoon/settings.py
+++ backend/tothemoon/tothemoon/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ega-m*3+h#qiqylf*ssrbz!4235+ek#&%tunc6e@&o&baxgtvg'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
